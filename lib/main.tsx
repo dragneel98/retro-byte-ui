312df4dee2fc1093d725fb7c2dc98390cc4e1da4
gonzalo mina <gonzalo.mina@hotmail.com.ar>
2024-08-21 19:22:17 -0300

    git commit -m "feat(main change): change the main component"

--- lib/main.tsx
+++ lib/main.tsx
@@ -8,7 +8,6 @@ export {Dropdown} from "./dropDown/dropDown"
 export {Pagination} from "./pagination/pagination"
 
 //cards
-
 export {ProductCard} from "./cards/product/product"
 export {UserCard} from "./cards/user/user"
 export {AdCard} from "./cards/adCart/adCard"
