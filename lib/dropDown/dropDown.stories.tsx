00f00c24422adbd5cf250679735440f9fbac7d53
gonzalo mina <gonzalo.mina@hotmail.com.ar>
2024-08-23 17:02:51 -0300

    fix: fix dropdown import

--- lib/dropDown/dropDown.stories.tsx
+++ lib/dropDown/dropDown.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import Dropdown from './dropDown';
+import {Dropdown} from './dropDown';
 
 const meta: Meta<typeof Dropdown> = {
   title: 'Components/Dropdown',
